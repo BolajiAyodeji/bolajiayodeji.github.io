82b540c374831b9fb6cca36b60a6c86fb3cc9688
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-08-10 20:20:37 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,6 +21,19 @@
         }
     });
 
+    //DROPDOWN MENU EFFECT
+    $(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
 
     // PARALLAX EFFECT
     $.stellar({
