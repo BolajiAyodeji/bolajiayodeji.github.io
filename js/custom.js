ff6189898ab242c6623cb9df219f3fd3ea7e2637
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-08-21 22:46:38 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -10,16 +10,7 @@
 
     
     //INTRO ANIMATION
-$$(document).ready(function() {
-    $('.Box').mousemove(function(e){
-        var window = $(this);
-        var x_axis = e.pageX - window.offset().left;
-        $('.mask').css({
-            left : x_axis,
-            backgroundPosition : '-' + x_axis + 'px -'
-        });
-    });
-});
+
     //END INTRO ANIMATION
     
     // MENU
