9ef447bd33004b1eb9bde61f763b0859d15396ee
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-10-05 16:26:22 +0100

    Added text animation

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,41 @@
     });
 
 
+    // WRAP INTRO TEXT
+$('.ml1 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml1 .letter',
+      scale: [0.3,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 600,
+      delay: function(el, i) {
+        return 70 * (i+1)
+      }
+    }).add({
+      targets: '.ml1 .line',
+      scaleX: [0,1],
+      opacity: [0.5,1],
+      easing: "easeOutExpo",
+      duration: 700,
+      offset: '-=875',
+      delay: function(el, i, l) {
+        return 80 * (l - i);
+      }
+    }).add({
+      targets: '.ml1',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+
+
     
     //INTRO ANIMATION
 
